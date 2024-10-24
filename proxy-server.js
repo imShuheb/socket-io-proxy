6882a31bc74ddfe7e7c845ec01c8030436065a40
Shuheb <mshuheb8@gmail.com>
2024-10-24 17:24:50 +0530

    first commit

--- proxy-server.js
+++ proxy-server.js
@@ -8,43 +8,27 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 app.get('/', (req, res) => {
-    res.send('Socket.IO Proxy running on port 3000. Use /ws1, /ws2, or /ws3 for WebSocket connections.');
+    res.send('Socket.IO Proxy running on port 3000');
 });
 
 io.of('/ws1').on('connection', (socket) => {
-    console.log('Client connected to proxy on /ws1');
-
     const targetSocket = Client('http://localhost:3001');
 
     socket.on('message', (msg) => {
-        console.log('Proxy received on /ws1:', msg);
         targetSocket.emit('message', msg);
     });
 
-    targetSocket.on('response', (msg) => {
-        console.log('Proxy forwarding response from /ws1:', msg);
-        socket.emit('response', msg);
-    });
-
     socket.on('disconnect', () => {
-        console.log('Client disconnected from proxy on /ws1.');
         targetSocket.disconnect();
     });
 });
 
 io.of('/ws2').on('connection', (socket) => {
-    console.log('Client connected to proxy on /ws2');
-
     const targetSocket = Client('http://localhost:3002');
 
     socket.on('message', (msg) => {
         console.log('Proxy received on /ws2:', msg);
         targetSocket.emit('message', msg);
-    });
-
-    targetSocket.on('response', (msg) => {
-        console.log('Proxy forwarding response from /ws2:', msg);
-        socket.emit('response', msg);
     });
 
     socket.on('disconnect', () => {
@@ -54,18 +38,12 @@ io.of('/ws2').on('connection', (socket) => {
 });
 
 io.of('/ws3').on('connection', (socket) => {
-    console.log('Client connected to proxy on /ws3');
 
     const targetSocket = Client('http://localhost:3003');
 
     socket.on('message', (msg) => {
         console.log('Proxy received on /ws3:', msg);
         targetSocket.emit('message', msg);
-    });
-
-    targetSocket.on('response', (msg) => {
-        console.log('Proxy forwarding response from /ws3:', msg);
-        socket.emit('response', msg);
     });
 
     socket.on('disconnect', () => {
